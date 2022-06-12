75a5f0215e671b21ec2099e09c8c1e6afb917689
RafisGit <106728315+RafisGit@users.noreply.github.com>
2022-06-12 10:43:21 +0530

    Add files via upload

---
+++ rafislab2_2.java
@@ -0,0 +1,25 @@
+
+import java.util.Scanner;
+public class rafislab2_2 {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+            System.out.println("Enter 3 numbers:");
+            int num1=input.nextInt();
+            int num2=input.nextInt();
+            int num3=input.nextInt();
+            
+            if(num1>num2 && num1>num3){
+                System.out.println("num1 is the largest number.");
+            }
+            else if(num2>num1 && num2>num3){
+                System.out.println("num2 is the largest number.");
+            }
+            else if(num3>num1 && num3>num2){
+                System.out.println("num3 is the largest number.");
+            }
+            else{
+                System.out.println("All 3 numbers are equal");
+            }
+            
+    }   
+}
